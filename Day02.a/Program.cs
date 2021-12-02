897aa89190244eeba2a32a5cc8a605cef0968435
Tera <24725862+teraa@users.noreply.github.com>
2021-12-02 08:55:27 +0100

    day 2

---
+++ Day02.a/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+int pos = 0, depth = 0;
+
+string? line;
+while ((line = Console.ReadLine()) is not null)
+{
+    var span = line.AsSpan();
+    int x = int.Parse(span[(span.IndexOf(' ') + 1)..]);
+
+    switch (span[0])
+    {
+        case 'd':
+            depth += x;
+            break;
+        case 'u':
+            depth -= x;
+            break;
+        default:
+            pos += x;
+            break;
+    }
+}
+
+Console.WriteLine(pos * depth);

bbafeed3097464d96fa2197eee3497f690ef68b5
Tera <24725862+teraa@users.noreply.github.com>
2021-12-03 17:06:14 +0100

    day 3

---
+++ Day03.a/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+const int len = 12;
+
+int[] ones = new int[len];
+int n = 0;
+string? line;
+
+while ((line = Console.ReadLine()) is not null)
+{
+    for (int i = 0; i < len; i++)
+        ones[i] += line[i] - '0';
+
+    n++;
+}
+
+int gamma = 0;
+int epsilon = 0;
+
+for (int i = 0; i < len; i++)
+{
+    int bit = 1 << (len - i - 1);
+
+    if (2 * ones[i] >= n)
+        gamma |= bit;
+    else
+        epsilon |= bit;
+}
+
+Console.WriteLine(gamma * epsilon);
---
+++ Day03.b/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+const int len = 12;
+
+List<int> nums = new();
+string? line;
+
+while ((line = Console.ReadLine()) is not null)
+    nums.Add(Convert.ToInt32(line, 2));
+
+int a = Find(nums, (x, y) => x != y);
+int b = Find(nums, (x, y) => x == y);
+
+Console.WriteLine(a * b);
+
+static int Find(IEnumerable<int> input, Func<int, int, bool> reduceCondition)
+{
+    List<int> subset = new(input);
+
+    for (int i = len - 1; i >= 0 && subset.Count > 1; i--)
+    {
+        int ones = 0;
+
+        for (int j = 0; j < subset.Count; j++)
+            ones += (subset[j] >> i) & 1;
+
+        int dominant = 2 * ones >= subset.Count ? 1 : 0;
+
+        subset.RemoveAll(x => reduceCondition(((x >> i) & 1), dominant));
+    }
+
+    Debug.Assert(subset.Count == 1);
+
+    return subset[0];
+}

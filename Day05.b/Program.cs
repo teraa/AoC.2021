fc3cd422ca2b32e545a4ae03a3994a5a54fb86e0
Tera <24725862+teraa@users.noreply.github.com>
2021-12-05 10:01:31 +0100

    day 5

---
+++ Day05.b/Program.cs
@@ -0,0 +1,87 @@
+﻿List<Line> lines = new();
+string? inputLine;
+
+while ((inputLine = Console.ReadLine()) is not null)
+{
+    ReadOnlySpan<char> span = inputLine;
+
+    int i = span.IndexOf(',');
+    int x1 = int.Parse(span[..i]);
+
+    span = span[(i + 1)..];
+
+    i = span.IndexOf(' ');
+    int y1 = int.Parse(span[..i]);
+
+    span = span[(i + " -> ".Length)..];
+
+    i = span.IndexOf(',');
+    int x2 = int.Parse(span[..i]);
+
+    span = span[(i + 1)..];
+
+    int y2 = int.Parse(span);
+
+    lines.Add(new(new(x1, y1), new(x2, y2)));
+}
+
+Dictionary<Point, int> hits = new();
+
+foreach (var line in lines)
+{
+    (int minx, int maxx) = (line.Start.X, line.End.X);
+    if (minx > maxx)
+        (minx, maxx) = (maxx, minx);
+
+    (int miny, int maxy) = (line.Start.Y, line.End.Y);
+    if (miny > maxy)
+        (miny, maxy) = (maxy, miny);
+
+    if (minx == maxx)
+    {
+        for (int y = miny; y <= maxy; y++)
+        {
+            Point point = new(minx, y);
+            Increment(point);
+        }
+    }
+
+    if (miny == maxy)
+    {
+        for (int x = minx; x <= maxx; x++)
+        {
+            Point point = new(x, miny);
+            Increment(point);
+        }
+    }
+
+    int dx = line.End.X - line.Start.X;
+    int dy = line.End.Y - line.Start.Y;
+
+    if (Math.Abs(dx) == Math.Abs(dy))
+    {
+        int len = Math.Abs(dx);
+        int ix = dx > 0 ? 1 : -1;
+        int iy = dy > 0 ? 1 : -1;
+
+        for (int i = 0; i <= len; i++)
+        {
+            int x = line.Start.X + i * ix;
+            int y = line.Start.Y + i * iy;
+            Point point = new(x, y);
+            Increment(point);
+        }
+    }
+}
+
+int result = hits.Count(x => x.Value > 1);
+
+Console.WriteLine(result);
+
+// ---
+
+void Increment(Point point)
+    => hits[point] = hits.TryGetValue(point, out int value) ? value + 1 : 1;
+
+record struct Point(int X, int Y);
+record struct Line(Point Start, Point End);

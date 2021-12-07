9daf4b3cf22e5f039b1fe2f61c245112998e0e27
Tera <24725862+teraa@users.noreply.github.com>
2021-12-07 10:12:05 +0100

    day 7

---
+++ Day07.a/Program.cs
@@ -0,0 +1,21 @@
+﻿ReadOnlySpan<char> input = Console.ReadLine();
+List<int> nums = new();
+
+int idx;
+while ((idx = input.IndexOf(',')) != -1)
+{
+    nums.Add(int.Parse(input[..idx]));
+    input = input[(idx + 1)..];
+}
+nums.Add(int.Parse(input));
+
+int max = nums.Max();
+int cost = int.MaxValue;
+for (int i = max; i >= 0; i--)
+{
+    int c = nums.Sum(x => Math.Abs(x - i));
+    if (cost > c)
+        cost = c;
+}
+
+Console.WriteLine(cost);

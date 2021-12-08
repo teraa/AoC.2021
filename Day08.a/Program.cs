c4e22aa77a3e9b47d0309f7578234734fd8dd695
Tera <24725862+teraa@users.noreply.github.com>
2021-12-08 23:46:16 +0100

    day 8

---
+++ Day08.a/Program.cs
@@ -0,0 +1,12 @@
+﻿int count = 0;
+string? line;
+
+while ((line = Console.ReadLine()) is not null)
+{
+    var parts = line[(line.IndexOf('|') + 2)..].Split(' ');
+    foreach (var part in parts)
+        if (part.Length is 2 or 3 or 4 or 7)
+            count++;
+}
+
+Console.WriteLine(count);
---
+++ Day08.b/Program.cs
@@ -0,0 +1,100 @@
+﻿const int mid = 59;
+const int digits = 10;
+
+long sum = 0;
+string? line;
+while ((line = Console.ReadLine()) is not null)
+{
+    int[] values = new int[digits];
+    int[] lengths = new int[digits];
+    int[] decoded = new int[digits];
+    Array.Fill(decoded, -1);
+
+    ReadOnlySpan<char> span = line;
+
+    var input = span[..(mid - 1)];
+    var output = span[(mid + 2)..];
+
+    for (int i = 0, j = 0; i < input.Length; i++)
+    {
+        if (input[i] is ' ')
+        {
+            j++;
+        }
+        else
+        {
+            values[j] |= 1 << (input[i] - 'a');
+            lengths[j]++;
+        }
+    }
+
+    // 1, 4, 7, 8 by length
+    for (int i = 0; i < lengths.Length; i++)
+    {
+        int j = lengths[i] switch
+        {
+            2 => 1,
+            3 => 7,
+            4 => 4,
+            7 => 8,
+            _ => -1,
+        };
+
+        if (j != -1)
+            decoded[j] = i;
+    }
+
+    var byLength = lengths.Select((len, idx) => (len, idx))
+        .GroupBy(x => x.len, x => x.idx)
+        .ToDictionary(x => x.Key, x => x.ToArray());
+
+    decoded[6] = byLength[6].First(x => GetWeight(values[x] & values[decoded[1]]) == 1);
+    decoded[9] = byLength[6].First(x => (values[x] & values[decoded[4]]) == values[decoded[4]]);
+    decoded[0] = byLength[6].First(x => x != decoded[6] && x != decoded[9]);
+
+    decoded[3] = byLength[5].First(x => (values[x] & values[decoded[1]]) == values[decoded[1]]);
+    decoded[2] = byLength[5].First(x => GetWeight(values[x] & values[decoded[4]]) == 2);
+    decoded[5] = byLength[5].First(x => x != decoded[3] && x != decoded[2]);
+
+    Debug.Assert(byLength[6].Where(x => GetWeight(values[x] & values[decoded[1]]) == 1).Count() == 1);
+    Debug.Assert(byLength[6].Where(x => (values[x] & values[decoded[4]]) == values[decoded[4]]).Count() == 1);
+    Debug.Assert(byLength[6].Where(x => x != decoded[6] && x != decoded[9]).Count() == 1);
+
+    Debug.Assert(byLength[5].Where(x => (values[x] & values[decoded[1]]) == values[decoded[1]]).Count() == 1);
+    Debug.Assert(byLength[5].Where(x => GetWeight(values[x] & values[decoded[4]]) == 2).Count() == 1);
+    Debug.Assert(byLength[5].Where(x => x != decoded[3] && x != decoded[2]).Count() == 1);
+
+    int[] outs = new int[4];
+    for (int i = 0, j = 0; i < output.Length; i++)
+    {
+        if (output[i] is ' ')
+            j++;
+        else
+            outs[j] |= 1 << (output[i] - 'a');
+    }
+
+    int result = 0;
+    foreach (var o in outs)
+    {
+        int i = Array.IndexOf(values, o);
+        int j = Array.IndexOf(decoded, i);
+        result = result * 10 + j;
+    }
+
+    sum += result;
+}
+
+Console.WriteLine(sum);
+
+static int GetWeight(int value)
+{
+    int result = 0;
+
+    for (int i = 0; i < digits; i++)
+    {
+        result += value & 1;
+        value >>= 1;
+    }
+
+    return result;
+}

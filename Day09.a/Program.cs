e6cf12432d9cd84cf2d3bc77cd477a899cadd9a8
Tera <24725862+teraa@users.noreply.github.com>
2021-12-09 15:29:04 +0100

    day 9

---
+++ Day09.a/Program.cs
@@ -0,0 +1,40 @@
+﻿List<string> input = new();
+
+string? line;
+while ((line = Console.ReadLine()) is not null)
+    input.Add(line);
+
+int sum = 0;
+
+for (int i = 0; i < input.Count; i++)
+{
+    for (int j = 0; j < input[i].Length; j++)
+    {
+        char c = input[i][j];
+        int x, y;
+
+        // up
+        (x, y) = (i - 1, j);
+        if (x >= 0 && input[x][y] <= c)
+            continue;
+
+        // down
+        (x, y) = (i + 1, j);
+        if (x < input.Count && input[x][y] <= c)
+            continue;
+
+        // left
+        (x, y) = (i, j - 1);
+        if (y >= 0 && input[x][y] <= c)
+            continue;
+
+        // right
+        (x, y) = (i, j + 1);
+        if (y < input[x].Length && input[x][y] <= c)
+            continue;
+
+        sum += input[i][j] - '0' + 1;
+    }
+}
+
+Console.WriteLine(sum);
---
+++ Day09.b/Program.cs
@@ -0,0 +1,46 @@
+﻿HashSet<(int, int)> processed = new();
+int[] topSizes = new int[3];
+List<string> input = new();
+
+string? line;
+while ((line = Console.ReadLine()) is not null)
+    input.Add(line);
+
+
+for (int i = 0; i < input.Count; i++)
+{
+    for (int j = 0; j < input[i].Length; j++)
+    {
+        int size = Process(i, j);
+        var min = topSizes.Select((v, i) => (v, i)).Min();
+        if (min.v < size)
+            topSizes[min.i] = size;
+    }
+}
+
+long prod = topSizes.Aggregate(1L, (a, v) => a * v);
+
+Console.WriteLine(prod);
+
+// ---
+
+int Process(int i, int j)
+{
+    if (!processed.Add((i, j))
+        || i < 0
+        || i >= input.Count
+        || j < 0
+        || j >= input[i].Length
+        || input[i][j] == '9')
+        return 0;
+
+    return 1
+        // up
+        + Process(i - 1, j)
+        // down
+        + Process(i + 1, j)
+        // left
+        + Process(i, j - 1)
+        // right
+        + Process(i, j + 1);
+}

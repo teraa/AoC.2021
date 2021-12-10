30f7b01005dadc7c9caa70e4c0a3c47a32d41a80
Tera <24725862+teraa@users.noreply.github.com>
2021-12-10 19:18:19 +0100

    day 10

---
+++ Day10.a/Program.cs
@@ -0,0 +1,36 @@
+﻿Dictionary<char, char> pairs = new()
+{
+    ['('] = ')',
+    ['['] = ']',
+    ['{'] = '}',
+    ['<'] = '>',
+};
+Stack<char> stack = new();
+long score = 0;
+
+string? line;
+while ((line = Console.ReadLine()) is not null)
+{
+    stack.Clear();
+    for (int i = 0; i < line.Length; i++)
+    {
+        if (pairs.ContainsKey(line[i]))
+        {
+            stack.Push(line[i]);
+        }
+        else if (pairs[stack.Pop()] != line[i])
+        {
+            score += line[i] switch
+            {
+                ')' => 3,
+                ']' => 57,
+                '}' => 1197,
+                '>' => 25137,
+                _ => throw new ArgumentException(),
+            };
+            break;
+        }
+    }
+}
+
+Console.WriteLine(score);
---
+++ Day10.b/Program.cs
@@ -0,0 +1,30 @@
+﻿Dictionary<char, (char c, int p)> pairs = new()
+{
+    ['('] = (')', 1),
+    ['['] = (']', 2),
+    ['{'] = ('}', 3),
+    ['<'] = ('>', 4),
+};
+List<long> scores = new();
+Stack<char> stack = new();
+
+string? line;
+while ((line = Console.ReadLine()) is not null)
+{
+    stack.Clear();
+
+    for (int i = 0; i < line.Length; i++)
+    {
+        if (pairs.ContainsKey(line[i]))
+            stack.Push(line[i]);
+        else if (pairs[stack.Pop()].c != line[i])
+            break;
+
+        if (i == line.Length - 1)
+            scores.Add(stack.Aggregate(0L, (a, v) => a * 5 + pairs[v].p));
+    }
+}
+
+long result = scores.OrderBy(x => x).ElementAt(scores.Count / 2);
+
+Console.WriteLine(result);

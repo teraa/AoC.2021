b87fec5530a3d06a919eb1348165860884dcf75e
Tera <24725862+teraa@users.noreply.github.com>
2021-12-13 12:37:12 +0100

    day 13

---
+++ Day13.a/Program.cs
@@ -0,0 +1,40 @@
+﻿List<(int x, int y)> points = new();
+int i;
+string? line;
+ReadOnlySpan<char> span;
+
+while ((line = Console.ReadLine()) is   { Length: > 0 })
+{
+    span = line;
+    i = span.IndexOf(',');
+    int x = int.Parse(span[..i]);
+    int y = int.Parse(span[(i + 1)..]);
+    points.Add((x, y));
+}
+
+i = "fold along ".Length;
+
+span = Console.ReadLine();
+char axis = span[i];
+int value = int.Parse(span[(i + 2)..]);
+
+Func<(int x, int y), bool> predicate;
+Func<(int x, int y), (int x, int y)> projection;
+
+if (axis == 'x')
+{
+    predicate = p => p.x > value;
+    projection = p => p with { x = 2 * value - p.x };
+}
+else
+{
+    predicate = p => p.y > value;
+    projection = p => p with { y = 2 * value - p.y };
+}
+
+var pointsToMove = points.Where(predicate).ToArray();
+points.RemoveAll(p => predicate(p));
+points.AddRange(pointsToMove.Select(projection));
+
+int result = points.Distinct().Count();
+Console.WriteLine(result);
---
+++ Day13.b/Program.cs
@@ -0,0 +1,50 @@
+﻿List<(int x, int y)> points = new();
+int i;
+string? line;
+
+while ((line = Console.ReadLine()) is { Length: > 0 })
+{
+    ReadOnlySpan<char> span = line;
+    i = span.IndexOf(',');
+    int x = int.Parse(span[..i]);
+    int y = int.Parse(span[(i + 1)..]);
+    points.Add((x, y));
+}
+
+i = "fold along ".Length;
+(int x, int y) max = (0, 0);
+
+while ((line = Console.ReadLine()) is not null)
+{
+    ReadOnlySpan<char> span = line;
+    char axis = span[i];
+    int value = int.Parse(span[(i + 2)..]);
+
+    Func<(int x, int y), bool> predicate;
+    Func<(int x, int y), (int x, int y)> projection;
+
+    if (axis == 'x')
+    {
+        max.x = value;
+        predicate = p => p.x > value;
+        projection = p => p with { x = 2 * value - p.x };
+    }
+    else
+    {
+        max.y = value;
+        predicate = p => p.y > value;
+        projection = p => p with { y = 2 * value - p.y };
+    }
+
+    var pointsToMove = points.Where(predicate).ToArray();
+    points.RemoveAll(p => predicate(p));
+    points.AddRange(pointsToMove.Select(projection));
+}
+
+for (int y = 0; y < max.y; y++)
+{
+    for (int x = 0; x < max.x; x++)
+        Console.Write(points.Contains((x, y)) ? '█' : ' ');
+
+    Console.WriteLine();
+}
